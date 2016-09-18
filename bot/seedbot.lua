139287a02ac2e1b900e6d53aa522316a7facadf2
mahdipilotts <mahdipilotmahdi@gmail.com>
2016-09-18 17:47:11 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {273899442,0,tonumber(our_id)},--Sudo users
+    sudo_users = {273899442,,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -249,7 +249,7 @@ Vamptacus
 
 Our channels:
 English: @TeleSeedCH
-Persian: @IranSeed
+Persian: @mahdipilot
 ]],
     help_text_realm = [[
 Realm Commands:
